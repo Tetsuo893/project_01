ca69ea82de0945c10c9ca326e41fa5083fb881ca
Tetsuo893 <jsp.illus@gmail.com>
2020-12-16 23:23:28 -0600

    create iptest.sh

---
+++ Linux/iptest.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# loop through list of IP address reanges from RockStar Corp with fping.
+
+iplist=(15.199.95.91/28
+15.199.94.91/28
+11.199.158.91/28
+167.172.144.11/32
+11.199.141.91/28)
+
+for ip in ${iplist[@]}
+        do
+        fping -g $ip >> testip.txt
+        done
+
